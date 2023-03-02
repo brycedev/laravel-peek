fccd9a796b9fe04b5407b8706a86ea526ee0a515
Bryce Jackson <bryce@brycedev.com>
2023-03-02 01:22:41 -0500

    properly configure webpack mix for vue 3

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,18 +1,17 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix')
+const path = require('path')
 
 mix.setPublicPath('public/vendor/laravel-peek')
-  .js('resources/js/app.js', 'js/')
-  .browserSync('localhost:9090')   // change this while developing in different host/port
+  .js('resources/js/app.js', 'js/').vue({ version: 3 })// change this while developing in different host/port
   .webpackConfig({
       output: {
-          publicPath: '/vendor/laravel-peek/',
-          chunkFilename: 'js/[name].js?id=[chunkhash]'
+        publicPath: '/vendor/laravel-peek/',
+        chunkFilename: 'js/[name].js?id=[chunkhash]'
       },
       resolve: {
-          alias: {
-              'vue$': 'vue/dist/vue.runtime.esm.js',
-              '@': path.resolve('resources/js'),
-          },
+        alias: {
+        '@': path.resolve('resources/js'),
+        },
       },
   })
   .sourceMaps()
